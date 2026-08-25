eb4af5b248ecc1008e79290e4cf222c0dbad7758
RaúlRivero <rdmrivero@gmail.com>
2026-08-24 21:58:44 -0300

    insert, update, delete

--- db_restaurante/usuariomodel.php
+++ db_restaurante/usuariomodel.php
@@ -10,9 +10,26 @@ class UsuarioModel {
 
         $sql = "SELECT * FROM usuarios WHERE id = ?";
         $stmt = $this->db->prepare($sql);
-
         $stmt->execute([$id]);
-
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function insert($nome, $email, $senha) {
+        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$nome, $email, $senha]);
+    }
+
+    public function update($id, $nome, $email, $senha) {
+        $sql = "UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$nome, $email, $senha, $id]);
+    }
+
+    public function delete($id) {
+        $sql = "DELETE FROM usuarios WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+
 }
